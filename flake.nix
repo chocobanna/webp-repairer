d47082e7229f522a6321ffd0373c57950470fa84
Kaden Cyster <kadencyster@gmail.com>
2026-05-25 15:48:08 -0700

    test repair work

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "WebP repair tools";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          libwebp
+          imagemagick
+          python3
+        ];
+      };
+    };
+}
